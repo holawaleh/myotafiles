d495626e1acc96a183fd519816b37babbe83faa8
holawaleh <holawaleh@gmail.com>
2026-01-04 09:19:57 +0100

    add ota_check view

--- backend/otaapp/views.py
+++ backend/otaapp/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
+
 from .serializers import FirmwareSerializer
 from .models import Firmware
+
 
 class FirmwareListCreate(APIView):
     def get(self, request):
@@ -15,4 +18,28 @@ class FirmwareListCreate(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def ota_check(request):
+    device = request.GET.get("device")
+    version = request.GET.get("version")
+
+    if not device or not version:
+        return JsonResponse({"update": False})
+
+    fw = (
+        Firmware.objects
+        .filter(device_type=device, is_active=True)
+        .order_by("-created_at")
+        .first()
+    )
+
+    if not fw or fw.version == version:
+        return JsonResponse({"update": False})
+
+    return JsonResponse({
+        "update": True,
+        "version": fw.version,
+        "url": request.build_absolute_uri(fw.bin_file.url),
+    })
