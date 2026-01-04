79f2000d2b0f7b52640f4fdbdb564b8536bd0786
holawaleh <holawaleh@gmail.com>
2026-01-04 19:21:47 +0100

    OTA logs created

--- backend/otaapp/urls.py
+++ backend/otaapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ota_check, FirmwareListCreate, ActivateFirmware
+from .views import (
+    ota_check,
+    FirmwareListCreate,
+    ActivateFirmware,
+    OTALogCreate,
+)
 
 urlpatterns = [
     path("ota/check/", ota_check),
+    path("ota/log/", OTALogCreate.as_view()),   # ← ADD THIS
     path("firmware/", FirmwareListCreate.as_view()),
     path("firmware/<int:firmware_id>/activate/", ActivateFirmware.as_view()),
 ]
